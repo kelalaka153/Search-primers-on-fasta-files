32cadac45dbf2328a51a82badfbbff30eab61f4b
kelalaka153 <46725068+kelalaka153@users.noreply.github.com>
2022-02-04 17:35:17 +0300

    Made the column size as parameter.

--- biostarSearch.pl
+++ biostarSearch.pl
@@ -2,9 +2,10 @@ use strict;
 use warnings;
 use Bio::SeqIO;
 
-my $usage = "perl biostarSearch.pl <fasta file> <primers file>";
+my $usage = "perl dnamotif.pl <fasta file> <primers file> <column size>";
 my $fasta_filename = shift(@ARGV) or die("Usage: $usage $!");
 my $pfile = shift(@ARGV) or die("Usage: $usage $!");
+my $csize = shift(@ARGV) or die("Usage: $usage $!");
 my $start = 0;
 my $motifCounter=1; 
 
@@ -26,7 +27,7 @@ while(my $seq_obj = $fasta_parser->next_seq())
         while((my $pos = index($seq_obj->seq(), $motif, $start)) != -1) {
 
             printf("\nmotif found at position %8d ", $pos + 1);
-            printf("[%8d,%8d]", ((($pos)/70))+1, (($pos) % 70)+1);
+            printf("[%8d,%8d]", ((($pos)/$csize))+1, (($pos) % $csize)+1);
             $start = $pos + 1;
         
         }
@@ -38,4 +39,5 @@ while(my $seq_obj = $fasta_parser->next_seq())
     close $info;
 
 }
+
 

356b525638bf097df7105f6e88bd1a8408ecdd46
kelalaka153 <46725068+kelalaka153@users.noreply.github.com>
2022-02-04 16:52:48 +0300

    Biostar perl implementation for searching
    Biostar perl implementation for searching fasta files

---
+++ biostarSearch.pl
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+use Bio::SeqIO;
+
+my $usage = "perl dnamotif.pl <fasta file> <primers file>";
+my $fasta_filename = shift(@ARGV) or die("Usage: $usage $!");
+my $pfile = shift(@ARGV) or die("Usage: $usage $!");
+my $start = 0;
+my $motifCounter=1; 
+
+my $fasta_parser = Bio::SeqIO->new(-file => $fasta_filename, -format => 'Fasta');
+
+
+
+while(my $seq_obj = $fasta_parser->next_seq())
+{
+    printf("Searching sequence '%s'...\n", $seq_obj->id);
+
+    open my $info, $pfile or die "Could not open primers $pfile: $!";
+
+    while( my $motif = <$info>)  {   
+        chomp $motif;
+        printf("\n[%2s Looking motif [%s]]\n", $motifCounter, $motif);    
+        $start = 0;
+
+        while((my $pos = index($seq_obj->seq(), $motif, $start)) != -1) {
+
+            printf("\nmotif found at position %8d ", $pos + 1);
+            printf("[%8d,%8d]", ((($pos)/70))+1, (($pos) % 70)+1);
+            $start = $pos + 1;
+        
+        }
+        
+        $motifCounter++;
+        printf("\n");
+    
+    }
+    close $info;
+
+}
+
